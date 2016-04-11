d08e6ca64e93606455086fdcb90b383a003da681
Rakesh Kumar <T_RakeshK3@verifone.com>
2016-04-11 08:35:28 +0100

    Solution for Simple Array Sum

---
+++ Algorithms/Warmup-Challenges/simple-array-sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+	int N = 0;
+	int S = 0;
+
+	std::cin >> N;
+
+	for (int i = 0; i < N; ++i) {
+		int V = 0;
+		std::cin >> V;
+		S += V;
+	}
+
+	std::cout << S << std::endl;
+
+	return 0;
+}
