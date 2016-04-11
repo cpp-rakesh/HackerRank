bd8d8fa5573530f010c256ec52e90ea170c996fe
Rakesh Kumar <T_RakeshK3@verifone.com>
2016-04-11 08:43:55 +0100

    Time Conversion Solution

---
+++ Algorithms/Warmup-Challenges/time-conversion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string T;
+	std::cin >> T;
+
+	if (T.substr(T.size() - 2, 2) == "PM" && T.substr(0, 2) != "12") {
+		T[0] += 1;
+		T[1] += 2;
+	}
+
+	if (T.substr(T.size() - 2, 2) == "AM" && T.substr(0, 2) == "12") {
+		T[0]  = T[1] = '0';
+	}
+
+	T.erase(T.size() - 2, 2);
+
+	std::cout << T << std::endl;
+
+	return 0;
+}
