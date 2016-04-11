f8a1aedd1c829c2f7a40fd8a41c4c652d4b34d07
Rakesh Kumar <T_RakeshK3@verifone.com>
2016-04-11 09:45:29 +0100

    Add Intro to tutorial challenge solution

---
+++ Algorithms/Sorting/intro_to_tuorial_challenges.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::vector<int> A;
+	int V = 0;
+	std::cin >> V;
+
+	int N = 0;
+	std::cin >> N;
+
+	while (N--) {
+		int value = 0;
+		std::cin >> value;
+		A.push_back(value);
+	}
+
+	int low = 0;
+	int high = A.size() - 1;
+	int mid = (low + high) / 2;
+	while (1) {
+		if (A[mid] == V) {
+			std::cout << mid << std::endl;
+			break;
+		} else if (A[mid] < V) {
+			low = mid + 1;
+			mid = (low + high) / 2;
+		} else {
+			high = mid - 1;
+			mid = (low + high) / 2;
+		}
+	}
+
+	return 0;
+}
