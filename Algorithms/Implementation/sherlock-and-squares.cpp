995e149753ef408532f59cc6a27280c43f663625
Rakesh Kumar <T_RakeshK3@verifone.com>
2016-04-11 08:57:44 +0100

    Solution for Sherlock and Squares

---
+++ Algorithms/Implementation/sherlock-and-squares.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cmath>
+
+int main() {
+	int T = 0;
+
+	scanf("%d", &T);
+
+	while (T--) {
+		int A = 0;
+		int B = 0;
+
+		scanf("%d %d", &A, &B);
+
+		printf("%d\n", static_cast<int>(floor(sqrt(B)) - ceil(sqrt(A))) + 1);
+	}
+
+
+	return 0;
+}
