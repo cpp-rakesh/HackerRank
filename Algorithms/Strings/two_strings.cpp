44886261ca205efe7c447d20799da3d1cda42c55
Rakesh Kumar <cpp.rakesh@gmail.com>
2016-04-12 01:40:25 +0100

    Two Strings solution

---
+++ Algorithms/Strings/two_strings.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	int T = 0;
+	std::cin >> T;
+
+	while (T--) {
+		std::string A;
+		std::string B;
+
+		std::cin >> A;
+		std::cin >> B;
+
+		bool is_found = false;
+
+		for (int i = 0; i < 26; ++i) {
+			const char c = i + 'a';
+			if (A.find(c) != std::string::npos &&
+				B.find(c) != std::string::npos) {
+				is_found = true;
+				break;
+			}
+		}
+
+		if (is_found)
+			std::cout << "YES" << std::endl;
+		else
+			std::cout << "NO" << std::endl;
+	}
+
+	return 0;
+}
