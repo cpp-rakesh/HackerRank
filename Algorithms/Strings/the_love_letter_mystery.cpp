cd2710c92227d9dc512702ab0d068ed90b9f5c6f
Rakesh Kumar <cpp.rakesh@gmail.com>
2016-04-12 01:24:22 +0100

    The Love Letter Mystery solution

---
+++ Algorithms/Strings/the_love_letter_mystery.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	int T = 0;
+	std::cin >> T;
+
+	while (T--) {
+		std::string S;
+		std::cin >> S;
+
+		int i = 0;
+		int j = S.length() - 1;
+		int c = 0;
+
+		while (i < j) {
+			if (S[i] != S[j]) {
+				if (S[i] > S[j]) {
+					while (S[i] != S[j]) {
+						--S[i];
+						++c;
+					}
+				} else {
+					while (S[i] != S[j]) {
+						--S[j];
+						++c;
+					}
+				}
+			}
+			++i;
+			--j;
+		}
+		std::cout << c << std::endl;
+	}
+
+	return 0;
+}
