6fe14c798e966d84debcc67518172baf2dda4934
cpp.rakesh <cpp.rakesh@gmail.com>
2016-07-05 05:59:59 +0100

    Solution for Super Reduced String

---
+++ Algorithms/Strings/super_reduced_string.cpp
@@ -0,0 +1,30 @@
+/*
+  Solution for Super Reduced from HackerRank.com
+  Author: Rakesh Kumar @ cpp.rakesh@gmail.com
+  Date: July 5th, 2016
+ */
+
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string data;
+	std::getline(std::cin, data);
+
+	int i = data.size() - 1;
+	while (i >= 0) {
+		if (data[i] == data[i - 1]) {
+			data.erase(i - 1, 2);
+			i = data.size() - 1;
+		} else {
+			--i;
+		}
+	}
+
+	if (data.size())
+		std::cout << data << std::endl;
+	else
+		std::cout << "Empty String" << std::endl;
+
+	return 0;
+}
