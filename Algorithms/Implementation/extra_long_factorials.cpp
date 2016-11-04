0866d06ad3f1bca95be9aa6a44f13abfb56569c5
cpp-rakesh <cpp.rakesh@gmail.com>
2016-11-05 02:03:50 +1100

    Optimized

--- Algorithms/Implementation/extra_long_factorials.cpp
+++ Algorithms/Implementation/extra_long_factorials.cpp
@@ -1,55 +1,57 @@
-#include <iostream>
-#include <deque>
+/*
+  Solution for Extra Long Factorial at Hacker Rank
+  Rakesh Kumar @ cpp.rakesh@gmail.com
+  Nov 4th, 2016
+ */
+
+#include <cstdio>
+#include <string>
 
 class Integer {
 public:
-	Integer(const int& val) {
-		m_data.push_back(val);
-	}
+    Integer(int value) {
+        while (value) {
+            m_data.push_back((value % 10) + '0');
+            value /= 10;
+        }
+    }
 
-	~Integer() {}
+    ~Integer() {}
 
-	void operator * (const int& value) {
-		int carry = 0;
-		std::size_t iter = 0;
-		while (iter < m_data.size()) {
-			const int val = (value * m_data[iter]) + carry;
-			if (val < 9) {
-				m_data[iter] = val;
-				carry = 0;
-			} else {
-				m_data[iter] = val % 10;
-				carry = val / 10;
-			}
-			++iter;
-		}
+    void operator * (int rhs) {
+        int carry = 0;
+        for (std::size_t i = 0; i < m_data.size(); ++i) {
+            const int sum = carry + (rhs * (m_data[i] - '0'));
+            m_data[i] = (sum % 10) + '0';
+            carry = sum / 10;
+        }
 
-		while (carry) {
-			m_data.push_back(carry % 10);
-			carry /= 10;
-		}
-	}
+        while (carry) {
+            m_data.push_back((carry % 10) + '0');
+            carry /= 10;
+        }
+    }
 
-	void Print() const {
-		for (int i = m_data.size() - 1; i >= 0; --i)
-			std::cout << m_data[i];
-		std::cout << std::endl;
-	}
+    void Print() const {
+        for (int i = m_data.size() - 1; i >= 0; --i)
+            printf("%c", m_data[i]);
+        printf("\n");
+    }
 
 private:
-	std::deque<int> m_data;
+    std::string m_data;
 };
 
 int main() {
-	Integer value(1);
+    Integer value(1);
 
-	int N = 0;
+    int N = 0;
 
-	std::cin >> N;
+    scanf("%d", &N);
 
-	for (int i = 2; i <= N; ++i)
-		value * i;
+    for (int i = 2; i <= N; ++i)
+        value * i;
 
-	value.Print();
-	return 0;
+    value.Print();
+    return 0;
 }
