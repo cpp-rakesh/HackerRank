db2c70ac69368abff006f96213a3f6e9438e4ff3
cpp-rakesh <cpp.rakesh@gmail.com>
2016-12-12 21:28:05 +1100

    Indendation

--- Algorithms/Sorting/intro_to_tuorial_challenges.cpp
+++ Algorithms/Sorting/intro_to_tuorial_challenges.cpp
@@ -1,35 +1,41 @@
+/*
+  Solution for Intro to Tutorial Challenges
+  Rakesh Kumar @ cpp.rakesh@gmail.com
+  Date: Dec 12th, 2016
+ */
+
 #include <iostream>
 #include <vector>
 
 int main() {
-	std::vector<int> A;
-	int V = 0;
-	std::cin >> V;
+    std::vector<int> A;
+    int V = 0;
+    std::cin >> V;
 
-	int N = 0;
-	std::cin >> N;
+    int N = 0;
+    std::cin >> N;
 
-	while (N--) {
-		int value = 0;
-		std::cin >> value;
-		A.push_back(value);
-	}
+    while (N--) {
+        int value = 0;
+        std::cin >> value;
+        A.push_back(value);
+    }
 
-	int low = 0;
-	int high = A.size() - 1;
-	int mid = (low + high) / 2;
-	while (1) {
-		if (A[mid] == V) {
-			std::cout << mid << std::endl;
-			break;
-		} else if (A[mid] < V) {
-			low = mid + 1;
-			mid = (low + high) / 2;
-		} else {
-			high = mid - 1;
-			mid = (low + high) / 2;
-		}
-	}
+    int low = 0;
+    int high = A.size() - 1;
+    int mid = (low + high) / 2;
+    while (1) {
+        if (A[mid] == V) {
+            std::cout << mid << std::endl;
+            break;
+        } else if (A[mid] < V) {
+            low = mid + 1;
+            mid = (low + high) / 2;
+        } else {
+            high = mid - 1;
+            mid = (low + high) / 2;
+        }
+    }
 
-	return 0;
+    return 0;
 }
