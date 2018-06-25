bfa831a044455df274bdcf0cfb6e0dd04358e8ac
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-06-25 23:28:36 +1000

    solution find the median

--- Algorithms/Sorting/find_the_median.cpp
+++ Algorithms/Sorting/find_the_median.cpp
@@ -4,3 +4,25 @@
   25/06/2018
  */
 
+#include <bits/stdc++.h>
+
+int findMedian(std::vector<int>& v) {
+    std::sort(v.begin(), v.end());
+    return v[v.size() >> 1];
+}
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+
+    std::vector<int> v;
+    while (n--) {
+        int e = 0;
+        scanf("%d", &e);
+        v.push_back(e);
+    }
+
+    printf("Median == [%d]\n", findMedian(v));
+
+    return 0;
+}
